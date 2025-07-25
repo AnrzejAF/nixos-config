36e2811a57e7dbcfdad0b1e9e5e6ebf559a60ce1
Anrzej <andrzejfijalo@gmail.com>
2025-07-25 13:23:19 +0200

    git init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "NixOS and Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    ) // {
+      nixosConfigurations.anrzej-nix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+
+        modules = [
+          ./hardware-configuration.nix
+          ./hosts/anrzej-nix.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.anrzej = import ./home/anrzej.nix;
+          }
+        ];
+      };
+    };
+}
---
+++ home/anrzej.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  home.username = "anrzej";
+  home.homeDirectory = "/home/anrzej";
+  home.stateVersion = "25.05";
+
+  programs.git = {
+    enable = true;
+
+    userName = "Anrzej";
+    userEmail = "andrzejfijalo@gmail.com";
+
+    aliases = {
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      st = "status";
+      lg = "log --oneline --graph --decorate";
+    };
+
+    extraConfig = {
+      core.editor = "nano";
+      color.ui = "auto";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    kdePackages.kate
+    htop
+  ];
+}
